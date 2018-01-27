a29b2ff8f09bc17ffa1aa29b158cecc8d64bd47a
Winston Chen <wenxinchen11@hotmail.com>
2018-01-26 22:17:53 -0500

    polished batch fn

--- utils.py
+++ utils.py
@@ -3,16 +3,13 @@ Python translated API of external trading service.
 Raises exceptions on any errors
 """
 from data_structures import Account
-
+import os
 import requests
 import matplotlib.pyplot as plt
+import pickle
 
 KEY = '0MJ5v-KSNMbINd3EB5H_Ew'
-<<<<<<< HEAD
-HOST = 'http://oec-2018.herokuapp.com/api/stock'
-=======
-HOST = 'http://oec-2018.herokuapp.com/api/'
->>>>>>> 31f905b85d26c7f7245cb5b8235f474e19a46985
+HOST = 'http://oec-2018.herokuapp.com/api'
 
 
 def list_tickers():
@@ -36,33 +33,49 @@ def sell_stock(ticker, num_shares):
     """Sell num_shares of given stock"""
 
 def get_account():
-"""Get account information
--> Account"""
-acc = Account()
-return acc
+    """Get account information
+    -> Account"""
+    acc = Account()
+    return acc
 
-def get_batch_prices():
+def get_batch_prices(update):
     """Get all historical prices for all current stocks"""
 
     d= {}
 
-    r = requests.get(HOST + "/list", params={"key":KEY}).json()
-    for ticker in r["stock_tickers"]:
-        r=requests.get(HOST, params={
-            "ticker":ticker,
-            "key":KEY
-        }).json()
-        d[ticker] = r["historical_price"]
-    
+    if os.path.isfile('batchprices.pickle') and not update:
+        with open('batchprices.pickle', 'rb') as handle:
+            d = pickle.load(handle)
+
+    else:
+        r = requests.get(HOST + "/stock/list", params={"key":KEY}).json()
+        for ticker in r["stock_tickers"]:
+            r=requests.get(HOST + "/stock", params={
+                "ticker":ticker,
+                "key":KEY
+            }).json()
+            d[ticker] = r["historical_price"]
+
+        with open('batchprices.pickle', 'wb') as handle:
+            pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
     i = 1
     for p in d:
+        plt.subplot(6,5,i)
         plt.plot(d[p])
         plt.ylabel(p)
-        plt.subplot(6,5,i)
+        if i == 2:
+            print(p)
         i += 1
 
+    print(i)
+    print(len(d))
+    plt.subplots_adjust(left=0.07, bottom=0.04, right=0.98, top=0.96,
+                wspace=0.4, hspace=0.4)
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
     plt.show()
-    
+        
 if __name__ == "__main__":
-    get_batch_prices()
+    get_batch_prices(False)
 

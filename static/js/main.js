32d1f852aa4e95bc98c8a50f046621e857897a93
Jerry Li <jerry@jerryli.ca>
2018-01-27 01:36:01 -0500

    dashboard update

--- static/js/main.js
+++ static/js/main.js
@@ -2,16 +2,16 @@ var key = "0MJ5v-KSNMbINd3EB5H_Ew";
 var host = "http://oec-2018.herokuapp.com/api";
 
 $( document ).ready( function() {
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+            columns: []
+        }
+    });
     // Get list of stocks
     $.getJSON(host+"/stock/list", {key: key}).done(function(data) {
         console.log(data);
         // Generate chart
-        var chart = c3.generate({
-            bindto: '#chart',
-            data: {
-                columns: []
-            }
-        });    
         // Get prices for each ticker and load in chart
         data["stock_tickers"].sort().forEach(ticker => {
             $("#buttons").append(`<button id="${ticker}">${ticker}</button>`);
@@ -37,6 +37,12 @@ $( document ).ready( function() {
             var data_string = `<div><code>${x["ticker"]}: ${x["market_value"]}, Δ:${change.toFixed(2)}%</code ></div>`;
             $("#holdings").append(data_string);
             total += x["market_value"];
+            $.getJSON(host+"/stock", {ticker: x["ticker"], key:key}).done(function(data) {
+                historical_price = [x["ticker"]].concat(data["historical_price"]);
+                chart.load({
+                    columns:[historical_price]
+                });
+            });
         });
         // Update total change
         $("#total").text(`${(((total - 10000000)/10000000)*100).toFixed(2)}%`);

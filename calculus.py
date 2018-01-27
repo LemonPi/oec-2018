4c00c48293de9b22d27842e7ed3cd9b4a1df0ecd
Johnson Zhong <johnsonzhong@hotmail.ca>
2018-01-27 00:15:53 -0500

    Format code using spaces

--- calculus.py
+++ calculus.py
@@ -5,18 +5,6 @@ import math
 import api
 
 
-class dfdt:
-    def first_d(prices, index, dx):
-        # computes first derivative = (f(x) - f(x-dx))/dx
-        # assumes index > 0
-        return (prices[index] - prices[index - 1]) / dx
-
-    def second_d(prices, index, dx):
-        # compute second derivative = (f(x) - 2f(x-dx) + f(x-2dx))/dx^2
-        # assumes index > 1
-        return (prices[index] - 2 * prices[index - 1] + prices[index - 2]) / (dx ** 2)
-
-
 def window_stats(historical_price, window, plot=False):
     times = range(0, len(historical_price))
     # data = pd.DataFrame(list(zip(times, historical_price)), columns=['Times', 'Price'])
@@ -45,71 +33,69 @@ def window_stats(historical_price, window, plot=False):
 
     return derivative.iloc[-1], second_deriv.iloc[-1]
 
-    
+
 def filter_out_smooth_stocks(smooth_threshold):
+    d = {}
+    with open('batchprices.pickle', 'rb') as handle:
+        d = pickle.load(handle)
 
-	d = {}
-	with open('batchprices.pickle', 'rb') as handle:
-		d = pickle.load(handle)
+    ok = []
 
-	ok = []
+    for tick in d:
+        s = pd.Series(d[tick])
+        if s.autocorr(lag=1) > 0.01:
+            print(tick, math.log(s.autocorr(lag=1)) * 100)
+        if math.log(s.autocorr(lag=1)) < -0.01:
+            ok.append(tick)
+    i = 1
+    for p in d:
+        plt.subplot(20, 10, i)
+        plt.plot(d[p])
+        plt.ylabel(p)
+        if i == 2:
+            print(p)
+        i += 1
 
-	for tick in d:
-		s = pd.Series(d[tick])
-		if s.autocorr(lag=1) > 0.01:
-			print(tick, math.log(s.autocorr(lag=1))*100)
-		if math.log(s.autocorr(lag=1)) < -0.01:
-			ok.append(tick)
-	i = 1
-	for p in d:
-		plt.subplot(20,10,i)
-		plt.plot(d[p])
-		plt.ylabel(p)
-		if i == 2:
-			print(p)
-		i += 1
+    print(i)
+    print(len(d))
+    plt.subplots_adjust(left=0.07, bottom=0.04, right=0.98, top=0.96,
+                        wspace=0.4, hspace=0.4)
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
+    plt.show()
 
-	print(i)
-	print(len(d))
-	plt.subplots_adjust(left=0.07, bottom=0.04, right=0.98, top=0.96,
-				wspace=0.4, hspace=0.4)
-	mng = plt.get_current_fig_manager()
-	mng.resize(*mng.window.maxsize())
-	plt.show()
 
-	
 def buy_or_sell():
+    d = {}
+    with open('batchprices.pickle', 'rb') as handle:
+        d = pickle.load(handle)
 
-	d = {}
-	with open('batchprices.pickle', 'rb') as handle:
-		d = pickle.load(handle)
+    prices = {}
+    mx = 0
+    balance = 100000
+    holdings = {}
 
-	prices = {}
-	mx = 0
-	balance = 100000
-	holdings = {}
+    for tick, p_list in d.items():
+        if len(p_list) > mx:
+            mx = len(p_list)
 
-	for tick, p_list in d.items():
-		if len(p_list) > mx:
-			mx = len(p_list)
+    for i in range(mx, 0, -1):
+        prices[i] = {}
 
-	for i in range(mx, 0, -1):
-		prices[i] = {}
+        for tick, p_list in d.items():
+            if len(p_list) > mx - i:
+                prices[i][tick] = p_list[-(mx - i) - 1]
 
-		for tick, p_list in d.items():
-			if len(p_list) > mx - i:
-				prices[i][tick] = p_list[-(mx-i)-1]
+    print(prices)
+    wym = []
+    for i in range(1, mx):
+        if "WYM" in prices[i]:
+            wym.append(prices[i]["WYM"])
 
-	print(prices)
-	wym = []
-	for i in range(1, mx):
-		if "WYM" in prices[i]:
-			wym.append(prices[i]["WYM"])
-
-	for i in range(1, mx):
-		pass
-		#put day-by-day sim here
-		# use holdings and balance vars
+    for i in range(1, mx):
+        pass
+        # put day-by-day sim here
+        # use holdings and balance vars
 
 
 if __name__ == "__main__":

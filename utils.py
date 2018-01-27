3484f5dac57b8c66a6cec3c676de93cc26063b6d
Winston Chen <wenxinchen11@hotmail.com>
2018-01-26 21:42:15 -0500

    added fn to display historical

--- utils.py
+++ utils.py
@@ -3,8 +3,11 @@ Python translated API of external trading service.
 Raises exceptions on any errors
 """
 
+import requests
+import matplotlib.pyplot as plt
+
 KEY = '0MJ5v-KSNMbINd3EB5H_Ew'
-HOST = 'http://oec-2018.herokuapp.com/api/stock/'
+HOST = 'http://oec-2018.herokuapp.com/api/stock'
 
 
 def list_tickets():
@@ -26,3 +29,28 @@ def buy_stock(stock_ticket, num_shares):
 
 def sell_stock(stock_ticket, num_shares):
     """Sell num_shares of given stock"""
+
+def get_batch_prices():
+    """Get all historical prices for all current stocks"""
+
+    d= {}
+
+    r = requests.get(HOST + "/list", params={"key":KEY}).json()
+    for ticker in r["stock_tickers"]:
+        r=requests.get(HOST, params={
+            "ticker":ticker,
+            "key":KEY
+        }).json()
+        d[ticker] = r["historical_price"]
+    
+    i = 1
+    for p in d:
+        plt.plot(d[p])
+        plt.ylabel(p)
+        plt.subplot(6,5,i)
+        i += 1
+
+    plt.show()
+    
+if __name__ == "__main__":
+    get_batch_prices()

4ea285372b24a1a5940694f6703d75c517d9805b
Johnson Zhong <johnsonzhong@hotmail.ca>
2018-01-26 21:31:09 -0500

    Fix typo

--- utils.py
+++ utils.py
@@ -7,22 +7,22 @@ KEY = '0MJ5v-KSNMbINd3EB5H_Ew'
 HOST = 'http://oec-2018.herokuapp.com/api/stock/'
 
 
-def list_tickets():
-    """List all stock tickets
+def list_tickers():
+    """List all stock tickers
     -> [string]"""
     return []
 
 
-def get_prices(stock_ticket):
+def get_prices(ticker):
     """Get historical prices (including current) of given stock in cents
     Last element in list is current price
     string -> [number]"""
     return 0
 
 
-def buy_stock(stock_ticket, num_shares):
+def buy_stock(ticker, num_shares):
     """Buy num_shares of given stock"""
 
 
-def sell_stock(stock_ticket, num_shares):
+def sell_stock(ticker, num_shares):
     """Sell num_shares of given stock"""
